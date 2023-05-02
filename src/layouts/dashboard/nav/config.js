8c1bb32dcebcba6903f1c926fa5a74deb7fbf493
ShrutiSolani <shrutiusolani@gmail.com>
2023-05-02 10:55:47 +0530

    Revert "upload milestone proof frontend"
    This reverts commit b37e5e220cd6a6f919011fe5c4444d5effa32b92.

--- src/layouts/dashboard/nav/config.js
+++ src/layouts/dashboard/nav/config.js
@@ -6,6 +6,21 @@ import SvgColor from '../../../components/svg-color';
 const icon = (name) => <SvgColor src={`/assets/icons/navbar/${name}.svg`} sx={{ width: 1, height: 1 }} />;
 
 const navConfig = [
+  // {
+  //   title: 'dashboard',
+  //   path: '/dashboard/app',
+  //   icon: icon('ic_analytics'),
+  // },
+  // {
+  //   title: 'user',
+  //   path: '/dashboard/user',
+  //   icon: icon('ic_user'),
+  // },
+  // {
+  //   title: 'product',
+  //   path: '/dashboard/products',
+  //   icon: icon('ic_cart'),
+  // },
   {
     title: 'My Campaigns',
     path: '/dashboard/mycampaigns',
@@ -27,9 +42,14 @@ const navConfig = [
     icon: icon('ic_blog'),
   },
   {
-    title: 'logout',
-    path: '/logout',
+    title: 'login',
+    path: '/login',
     icon: icon('ic_lock'),
+  },
+  {
+    title: 'Not found',
+    path: '/404',
+    icon: icon('ic_disabled'),
   },
 ];
 
